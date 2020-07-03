5c04bb4d9c6936216f92d65edeee232d6e312099
Kreft Dawid <Dawid.Kreft@pentacomp.pl>
2020-07-03 11:34:32 +0200

    Initiaization of basic structure

---
+++ b_microservice/src/main/java/com/example/demo/BMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BMicroserviceApplication.class, args);
+	}
+
+}
