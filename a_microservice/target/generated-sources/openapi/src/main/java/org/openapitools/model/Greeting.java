f37a8ccb84a67f74167fa29435f1f1eff57a0101
Kreft Dawid <Dawid.Kreft@pentacomp.pl>
2020-07-03 17:16:54 +0200

    open api with generated code

---
+++ a_microservice/target/generated-sources/openapi/src/main/java/org/openapitools/model/Greeting.java
@@ -0,0 +1,83 @@
+package org.openapitools.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.io.Serializable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Greeting
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-07-03T17:13:44.884927+02:00[Europe/Warsaw]")
+
+public class Greeting  implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @JsonProperty("name")
+  private String name;
+
+  public Greeting name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+  */
+  @ApiModelProperty(value = "")
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Greeting greeting = (Greeting) o;
+    return Objects.equals(this.name, greeting.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Greeting {\n");
+    
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
